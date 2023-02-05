4777e368de10bf5bc49a7e8e6e83e190519b2004
na.wei <na.wei2000@outlook.com>
2023-02-05 22:03:46 +0800

    更改登陆页面背景图片以及对登录添加验证信息，提示用户名密码不为空

--- fronted/src/view/Login/Login.vue
+++ fronted/src/view/Login/Login.vue
@@ -1,67 +1,122 @@
 <script setup lang="ts">
-    import {ref,reactive} from 'vue';
+    import {ref, reactive} from 'vue';
     import {userLogin} from "../../api/loginApi";
     import router from "../../router/index";
 
     const formData = reactive({
-        username:'',
-        password:''
+        username: '',
+        password: ''
     })
+    const rules = reactive({
+        username: [
+            {required: true, message: '请输入用户名', trigger: 'blur'},
+        ],
+        password: [
+            {
+                required: true,
+                message: '请输入密码',
+                trigger: 'blur',
+            },
+        ],
+    })
+    const ruleFormRef = ref();
     const sumbit = () => {
-        userLogin({
-            username:formData.username,
-            password:formData.password
-        }).then((res) => {
-            if( res.status === 200){
-                console.log(res.data.length)
-                if(formData.username === res.data[0].username && formData.password === res.data[0].password) {
+        ruleFormRef.value.validate((valid: any) => {
+            if (valid) {
+                console.log(valid)
+                userLogin({
+                    username: formData.username,
+                    password: formData.password
+                }).then((res) => {
+                    if (res.status === 200) {
+                        console.log(res.data.length)
+                        if (formData.username === res.data[0].username && formData.password === res.data[0].password) {
+                            // @ts-ignore
+                            ElMessage({
+                                message: '登录成功',
+                                type: 'success',
+                            })
+                            router.push('/main')
+                        }
+                    }
+                }).catch((err) => {
                     // @ts-ignore
                     ElMessage({
-                        message: '登录成功',
-                        type: 'success',
+                        message: '账户或密码填写错误，请重新填写',
+                        type: 'error',
                     })
-                    router.push('/main')
-                }
+                })
+            } else {
+                console.log(valid)
+                return false
             }
-        }).catch((err) =>{
-            // @ts-ignore
-            ElMessage({
-                message: '账户或密码填写错误，请重新填写',
-                type: 'error',
-            })
         })
     }
 </script>
 <template>
     <div class="container">
         <div class="login-container">
-            <el-form :model="formData">
-                <el-form-item required>
-                    <template #label>
-                        用户名
-                    </template>
-                    <el-input placeholder="请输入用户名" v-model="formData.username"></el-input>
-            </el-form-item>
-                <el-form-item required>
-                    <template #label>
-                     密&nbsp;&nbsp;&nbsp;码
-                    </template>
-                    <el-input placeholder="请输入密码" v-model="formData.password"></el-input>
-                </el-form-item>
-                <el-button @click="sumbit">登录</el-button>
-            </el-form>
+            <div class="title">桂湖大药房连锁药房管理系统</div>
+            <div class="login-form">
+                <el-form :model="formData" ref="ruleFormRef" :rules="rules">
+                    <el-form-item required prop="username">
+                        <template #label>
+                            用户名
+                        </template>
+                        <el-input placeholder="请输入用户名" v-model="formData.username"></el-input>
+                    </el-form-item>
+                    <el-form-item required prop="password">
+                        <template #label>
+                            密&nbsp;&nbsp;&nbsp;码
+                        </template>
+                        <el-input placeholder="请输入密码" v-model="formData.password"></el-input>
+                    </el-form-item>
+                    <el-form-item>
+                        <el-button @click="sumbit" type="primary">登录</el-button>
+                    </el-form-item>
+                </el-form>
+            </div>
         </div>
     </div>
 </template>
 
 
 <style lang="scss" scoped>
-.container{
-    height: 100%;
-    width: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    background-image: linear-gradient(to top, #1c9cf9,#0082fa);
-}
+    .container {
+        height: 100%;
+        width: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        background-color: aliceblue;
+        background-repeat: no-repeat;
+        background-position-y: 50%;
+        background-image: url("https://img1.baidu.com/it/u=1499999492,3493837702&fm=253&fmt=auto&app=138&f=PNG?w=857&h=500");
+
+        .login-container {
+            margin-left: 40%;
+            width: 500px;
+            height: 500px;
+            flex-direction: column;
+            display: flex;
+            align-items: center;
+            justify-content: flex-start;
+            box-shadow: 2px 3px 10px 1px lightgray;
+
+            .title {
+                margin-top: 25%;
+                font-size: 30px;
+                font-family: YouYuan;
+            }
+
+            .login-form {
+                margin-top: 10%;
+
+                .el-button {
+                    width: 300px;
+                }
+            }
+
+        }
+    }
 </style>
